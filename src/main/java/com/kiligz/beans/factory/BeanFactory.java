faa01d79b833ec72cf40dcd28badc19f2a903357
zhuyifan10045 <zyfkilig@gmail.com>
2022-08-12 13:19:27 +0800

    ioc-01

--- src/main/java/com/kiligz/beans/factory/BeanFactory.java
+++ src/main/java/com/kiligz/beans/factory/BeanFactory.java
@@ -3,6 +3,9 @@ package com.kiligz.beans.factory;
 import com.kiligz.beans.BeansException;
 
 /**
+ * bean工厂
+ * 定义获取bean的方法
+ *
  * @author Ivan
  * @date 2022/8/11 17:22
  */
--- src/main/java/com/kiligz/beans/factory/config/BeanDefinition.java
+++ src/main/java/com/kiligz/beans/factory/config/BeanDefinition.java
@@ -1,7 +1,8 @@
 package com.kiligz.beans.factory.config;
 
 /**
- * 定义bean信息的类，包含bean的class类型、构造参数、属性值等信息，每个bean对应一个
+ * 定义bean信息的类
+ * 包含bean的class类型、构造参数、属性值等信息，每个bean对应一个
  *
  * @author Ivan
  * @date 2022/8/11 17:18
